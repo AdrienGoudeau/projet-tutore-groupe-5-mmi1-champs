a12da8770bcac7f54213b1e953ebe4ab54a69a29
Ghatos <marwan.ghatos.akkari@gmail.com>
2021-05-11 00:45:12 +0200

    nouveau plugin: velocity.js
    nouveau plugin pour résoudre certains problèmes avec la fonction .animate de jquery et la fonction position du CSS

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-$( document ).ready(function() {
-    $("div").click(function()
-    {
-        $("div").animate({left: "25vw"});
-        $("").hide(1000);
-        {
-            $("div").animate({top: "25vh"});
-            $("").hide(1000);
-            {
-                $("div").animate({left: "0vw"});
-                $("").hide(1000);
-                {
-                    $("div").animate({top: "0vh"});
-                }
-            }
+$(document).ready(function() {
+    $(document).mousedown(function(e){
+        if(e.which === 2 ){ 
+           return false; // Or e.preventDefault()
         }
     });
+    $("img").click(function()
+    {
+        $("img").velocity({
+            width: "170vw",
+            height: "170vh",
+            top: "-50vh"
+            },{
+              duration: 1000,
+              easing: "ease"
+            })
+    })
 });
