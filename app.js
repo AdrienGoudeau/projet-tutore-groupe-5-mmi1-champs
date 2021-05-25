893fc2cf237106b1072d103e85907707e3277011
Ghatos1 <marwan.ghatos.akkari@gmail.com>
2021-05-25 15:31:51 +0200

    f
    f

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ $(document).ready(function() {
            return false; // Or e.preventDefault()
         }
     });
-    $(".ileDesign").velocity({
-        width: "80vw",
-        height: "80vh",
-        top: "10vh"
-    },{
-          duration: 1000,
-          easing: "ease-in-out"
-    })
-    $(".ileDesign").click(function()
-    {
-        $("img").velocity({
-            width: "170vw",
-            height: "170vh",
-            top: "-50vh"
-            },{
-              duration: 1000,
-              easing: "ease-in-out"
-            })
-    })
+//     $(".ileDesign").velocity({
+//         width: "80vw",
+//         height: "80vh",
+//         top: "10vh"
+//     },{
+//           duration: 1000,
+//           easing: "ease-in-out"
+//     })
+//     $(".ileDesign").click(function()
+//     {
+//         $("img").velocity({
+//             width: "170vw",
+//             height: "170vh",
+//             top: "-50vh"
+//             },{
+//               duration: 1000,
+//               easing: "ease-in-out"
+//             })
+//     })
 });
