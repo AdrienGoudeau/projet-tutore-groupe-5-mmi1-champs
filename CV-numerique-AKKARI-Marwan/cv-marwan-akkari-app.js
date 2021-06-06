8e60377a505ec78c293b491c8763faf8b3c072a8
AdrienGoudeau <60601152+AdrienGoudeau@users.noreply.github.com>
2021-06-06 23:33:16 +0200

    CV(s)

---
+++ CV-numerique-AKKARI-Marwan/cv-marwan-akkari-app.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    var infoPos = 0;
+    $(".goRight").on('click', function()
+    {
+        if (infoPos != 3)
+        {
+            $(".bottom-container").transition({x:'-=100vw'},700,'ease');
+            infoPos += 1;
+        }
+        else
+        {
+            $(".bottom-container").transition({x:'0vw'},700,'ease');
+            infoPos = 0;
+        }
+    })
+    $(".goLeft").on('click', function()
+    {
+        if (infoPos != 0)
+        {
+            $(".bottom-container").transition({x:'+=100vw'},700,'ease');
+            infoPos -= 1;
+        }
+        else
+        {
+            $(".bottom-container").transition({x:'-300vw'},700,'ease');
+            infoPos = 3;
+        }
+    })
+})
