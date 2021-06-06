e5012e0d613b717a4983cb81f5bfa021e26da97f
AdrienGoudeau <60601152+AdrienGoudeau@users.noreply.github.com>
2021-06-06 14:03:34 +0200

    add  connexion_base

---
+++ connexion_base.php
@@ -0,0 +1,8 @@
+<?php
+
+
+$link = new PDO('mysql:host=localhost;dbname=projet_tut_2021', 'root', '', array
+(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+// pour le serveur de l'UPEM, remplacer localhost par sqletud.u-pem.fr
+
+?>
