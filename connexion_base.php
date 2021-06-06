af7b71beac8dba32b4017577a49fea8d41e7a00d
AdrienGoudeau <60601152+AdrienGoudeau@users.noreply.github.com>
2021-06-07 01:27:05 +0200

    MaJ nom Db

--- connexion_base.php
+++ connexion_base.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$link = new PDO('mysql:host=sqletud.u-pem.fr;dbname=projet_tut_2021', 'agoudeau_db', 'PCSCGIFA18a', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+$link = new PDO('mysql:host=sqletud.u-pem.fr;dbname=agoudeau_db', 'agoudeau', 'PCSCGIFA18a', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 // pour le serveur de l'UPEM, remplacer localhost par sqletud.u-pem.fr
 
 ?>
